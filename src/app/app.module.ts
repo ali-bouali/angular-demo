24d9ce32b823936a924a0e3f0877d7f90ca5a292
ali-bouali <bouali.pro@gmail.com>
2024-06-29 11:08:06 +0200

    angular initiation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyFirstComponent } from './demo/my-first/my-first.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // ndeclari les composants mte3i hna
+    AppComponent, MyFirstComponent
   ],
-  imports: [
+  imports: [ // na3mel import l ayy module nest7a99ou
     BrowserModule,
     AppRoutingModule
   ],
+  // lazemni n7ott les services elli mahomech privded in root
   providers: [],
+  // l composant el kbir elli bech ndemarri bih l application
   bootstrap: [AppComponent]
 })
 export class AppModule { }
